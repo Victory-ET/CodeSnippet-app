666baa70e8d5e97c832c790eccd7aa85f7eb49f5
Victory <tuduov@gmail.com>
2021-11-03 00:03:29 +0100

    Update Fauna.js

--- Fauna.js
+++ Fauna.js
@@ -6,7 +6,7 @@ const q = faunadb.query;
 const getResponse = async () => {
   const { data } = await faunaClient.query(
     q.Map(
-      q.Paginate(q.Documents(q.Collection("snippet"))),
+      q.Paginate(q.Documents(q.Collection("codesnippet"))),
       q.Lambda("doc", q.Get(q.Var("doc")))
     )
   );
@@ -19,26 +19,26 @@ const getResponse = async () => {
 };
 
 const createSnippet = async (code, language, description, name, mail) => {
-  return await faunaClient.query(q.Create(q.Collection('snippet'), {
+  return await faunaClient.query(q.Create(q.Collection('codesnippet'), {
     data:{code, language, description, name, mail}
   }))
 }
 
 const updateSnippet = async (id, code, language, description, name) => {
-  return await faunaClient.query(q.Update(q.Ref(q.Collection('snippet'), id), {
+  return await faunaClient.query(q.Update(q.Ref(q.Collection('codesnippet'), id), {
     data: {code, language, name, description},
   }))
 }
 
 const getSnippetById = async (id) => {
-  const snippet = await faunaClient.query(q.Get(q.Ref(q.Collection('snippet'),id)))
+  const snippet = await faunaClient.query(q.Get(q.Ref(q.Collection('codesnippet'),id)))
   snippet.id = snippet.ref.id;
   delete snippet.ref;
   return snippet;
 }
 
 const deleteSnippet = async (id) => {
-  return await faunaClient.query(q.Delete(q.Ref(q.Collection('snippet'),id)))
+  return await faunaClient.query(q.Delete(q.Ref(q.Collection('codesnippet'),id)))
 }
 
 
